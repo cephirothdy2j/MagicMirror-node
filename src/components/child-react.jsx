6872a7680cb397cfbe9893d398b73d229927b613
Dylan Schuster <dylan.schuster@zipscene.com>
2016-01-03 22:48:42 -0500

    moving components to separate files

---
+++ src/components/child-react.jsx
@@ -0,0 +1,10 @@
+var React = require('../js/react');
+ChildReact = React.createClass({
+	render: function() {
+		return (
+			<p>foobarda</p>
+		);
+	}
+});
+
+module.exports = ChildReact;
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,26 @@
- var jquery = require('jquery');
- var React = require('react');
- var ReactDOM = require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
- 
+// require all the modules
+require('../components/child-react.jsx');
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+    	<div>
+    	<ChildReact />
+      <p>
+        Hello, <input type="text" placeholder="Your name here" />!
+        It is {this.props.date.toTimeString()}
+      </p>
+      </div>
+    );
+  }
+});
+
+setInterval(function() {
+  ReactDOM.render(
+    <HelloWorld date={new Date()} />,
+    document.getElementById('example')
+  );
+}, 500);
